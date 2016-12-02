df7b1333571dcca7d720b700980c5301c2acdb5e
Jake Worth <jake@jakeworth.com>
2016-12-01 21:10:32 -0600

    Remove `trace` -- ExUnit handles nicely on its own

--- test/hello_world_test.exs
+++ test/hello_world_test.exs
@@ -2,7 +2,6 @@ defmodule HelloWorldTest do
   use ExUnit.Case
 
   ExUnit.start
-  ExUnit.configure trace: true
 
   test "says hello sample name" do
     assert HelloWorld.hello("Alice") == "Hello, Alice!"
