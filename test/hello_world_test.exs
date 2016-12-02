a4d7dff1fa128da384e2d74a7335ab222e772c48
Jake Worth <jake@jakeworth.com>
2016-12-01 21:09:01 -0600

    Remove option for tests to be pending

--- test/hello_world_test.exs
+++ test/hello_world_test.exs
@@ -2,7 +2,7 @@ defmodule HelloWorldTest do
   use ExUnit.Case
 
   ExUnit.start
-  ExUnit.configure exclude: :pending, trace: true
+  ExUnit.configure trace: true
 
   test "says hello sample name" do
     assert HelloWorld.hello("Alice") == "Hello, Alice!"
